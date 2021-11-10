133817d363b99439928bbe41ca13ae16dc606e6c
dex1g <dexopl@gmail.com>
2021-11-10 20:22:41 +0100

    new frontend project on vite

---
+++ frontend/app/src/components/ChartTwoDim.vue
@@ -0,0 +1,35 @@
+<template>
+  <div ref="chart" class="chart"></div>
+</template>
+
+<script setup lang="ts">
+  import { ref, watchEffect } from 'vue'
+  import Plotly from 'plotly.js-basic-dist'
+
+  const chart = ref<HTMLElement>()
+
+  const chartData = ref<Plotly.Data[]>([
+    {
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16],
+      mode: 'markers',
+      type: 'scatter',
+    },
+  ])
+
+  watchEffect(() => {
+    if (chart.value) {
+      Plotly.newPlot(chart.value, chartData.value, {
+        margin: { t: 0 },
+      })
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+  .chart {
+    width: 800px;
+    height: 600px;
+    margin: auto;
+  }
+</style>
--- frontend/app/src/main.ts
+++ frontend/app/src/main.ts
@@ -1,10 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp(App).use(router);
-app.use(VueAxios, axios)
-app.provide('axios', app.config.globalProperties.axios)  // provide 'axios'
+const app = createApp(App)
+app.provide('apiUrl', import.meta.env.VITE_APP_API_URL)
 app.mount('#app')
